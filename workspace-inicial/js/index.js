2f2637d7eec5cff3e3ae38e49be0ceb95d7d0f20
Abigail Ormeño <Abigail.ormc@gmail.com>
2022-11-20 17:55:11 -0300

    Entrega 7

--- workspace-inicial/js/index.js
+++ workspace-inicial/js/index.js
@@ -1,14 +1,9 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
-});
+if (localStorage.getItem('email')) {
+    let newEmail = localStorage.getItem('email')
+    document.getElementById('email').innerHTML = newEmail;
+}
+
+function cerrarSesion(e) {
+    localStorage.removeItem('email');
+    window.location = "index.html"
+}
--- workspace-inicial/js/my-profile.js
+++ workspace-inicial/js/my-profile.js
@@ -1,9 +1,38 @@
+let profileForm = document.getElementById("profile-form")
+
 if (localStorage.getItem('email')) {
     let newEmail = localStorage.getItem('email')
-    document.getElementById('email').innerHTML = newEmail;
-}
+    document.getElementById('mail').value = newEmail;
 
-function cerrarSesion(e) {
-    localStorage.removeItem('email');
+    if (localStorage.getItem('profileValues')){
+        let profileValues = JSON.parse(localStorage.getItem('profileValues'))
+        document.getElementById('name').value = profileValues.name;
+        document.getElementById('secName').value = profileValues.secName;
+        document.getElementById('lastname').value = profileValues.lastname;
+        document.getElementById('secLastname').value = profileValues.secLastname;
+        document.getElementById('number').value = profileValues.number;        
+    }
+
+}else{
     window.location = "index.html"
 }
+
+function saveValues() {
+    let name = document.getElementById("name").value
+    let secName = document.getElementById("secName").value
+    let lastname = document.getElementById("lastname").value
+    let secLastname = document.getElementById("secLastname").value
+    let number = document.getElementById("number").value
+
+    let profileValues = {
+        name,
+        secName,
+        lastname,
+        secLastname,
+        number,
+    }
+    window.localStorage.setItem("profileValues", JSON.stringify(profileValues));
+    
+}
+
+profileForm.addEventListener('submit', saveValues);
